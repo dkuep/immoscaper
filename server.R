52b42a6ff49cea57c1d915137a885b884a5f213c
dkuep <dennis.kueppers@gmail.com>
2018-02-18 20:15:19 +0100

    single file app aufgeteilt in server.r und ui.r

--- server.R
+++ server.R
@@ -1,23 +1,74 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
-shinyServer(function(input, output) {
+source("~/Desktop/immoscape/immoscaper/settings.r")
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+  
+  observeEvent(input$delete,{
+    if(input$okdelete){
+      file.remove(paste0(dir,input$srcfile))
+      updateSelectInput(session,'srcfile', choices = list.files(dir))
+      updateCheckboxInput(session, 'okdelete', value = F)
+      showNotification("Gelöscht", type = 'warning', duration = 10)
+    }
+    else
+      showNotification("Löschen nicht freigeschaltet", type = 'warning', duration = 10)
   })
-
+  
+  get_data <- eventReactive(input$run,{
+    
+    progress <- shiny::Progress$new()
+    progress$set(message = "Lade Daten...", value = 0)
+    if(!input$getgeo) progress$set(value = 0.3)
+    on.exit(progress$close())
+    
+    path = paste0(dir,input$srcfile)
+    
+    if(input$data_qual == 'update'){
+      data <- load_immo_data(path, input$original)
+      if(input$save)
+        save_data(data, path, appendix = '_update')
+    }
+    else if(input$data_qual == 'new')
+      data <- process_immo(src = path,
+                           original = input$original,
+                           get_geo = input$getgeo,
+                           save_update = input$save, 
+                           geo_progress = progress
+      )
+    if(input$save) showNotification("Update gespeichert", type = 'message', duration = 10)
+    progress$set(value = 1)
+    
+    updateSelectInput(session,'srcfile', choices = list.files(dir))
+    updateCheckboxGroupInput(session, "table1_cols",
+                             choices = names(data), 
+                             selected = table1_cols_selected
+    )
+    updateSelectInput(session, 'spiegel_group', choices = names(data), selected = 'typ')
+    
+    return(data)
+  })
+  
+  output$table1 <- renderDataTable({
+    get_data()[,input$table1_cols]
+  }, escape = F)
+  
+  
+  output$info <- renderPrint({
+    get_data() %>% names()
+  })
+  
+  
+  output$plot2 <- renderPlot({
+    dat = get_data()
+    dat$spiegel <- dat$kalt / dat$flaeche
+    ggplot(dat) +
+      aes(y=dat$spiegel, x = as.factor(dat[,input$spiegel_group])) +
+      geom_boxplot() +
+      ylab("kalt / Fläche") +
+      xlab(input$spiegel_group) +
+      ggtitle("Mietspiegel")
+  })
 })
+# shinyApp(ui = ui, server = server)
+
--- ui.R
+++ ui.R
@@ -1,30 +1,89 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
-shinyUI(fluidPage(
+source("~/Desktop/immoscape/immoprocess/process2.r")
+source("~/Desktop/immoscape/immoprocess/get_longlat.r")
+source("~/Desktop/immoscape/immoprocess/controll.r")
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+source("~/Desktop/immoscape/immoscaper/settings.r")
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+shinyUI(
+  navbarPage('ImmoScape - nerdiger Immobilien Explorer',
+             tabPanel("Setup",
+                      fluidPage(
+                        fluidRow(
+                          column(4,
+                                 selectInput('srcfile', paste0(dir,'...'), choices = list.files(dir)), # update!!
+                                 radioButtons('data_qual',label = NULL,
+                                              choices = c('neue Daten' = 'new', 'verarbeitete Daten' = 'update'), 
+                                              selected = 'new'),
+                                 actionButton('run', 'Los gehts!', icon = icon("ok", lib = "glyphicon"))
+                          ), # column
+                          column(4,               
+                                 checkboxInput('original', 'Unverarbeitet einlesen'),
+                                 checkboxInput('save', 'Ergebnis speichern?'),
+                                 checkboxInput('getgeo', 'fehlende Geodaten holen?')
+                          ), # column
+                          column(4,
+                                 checkboxInput('okdelete', 'wirklich Löschen'),
+                                 actionButton('delete','Löschen', icon = icon('trash'))
+                          ) # column
+                        ), # fluidRow
+                        hr(),
+                        fluidRow(
+                          column(2,
+                                 checkboxGroupInput("table1_cols", "Auswahl:",  # update!!
+                                                    names(data),  selected = table1_cols_selected)
+                          ),
+                          column(10, dataTableOutput("table1"))
+                        )
+                      ) # fluidPage
+                      
+             ), # tabPanel
+             tabPanel( "Mietspiegel",
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput('spiegel_group', 'Gruppieren',    # update!!
+                                       names(data),selected = "typ")
+                         ),
+                         mainPanel(
+                           plotOutput('plot2'),
+                           verbatimTextOutput("info")
+                         )
+                       )
+             ), # tabPanel
+             tabPanel("Plot",
+                      sidebarLayout(sidebarPanel(
+                        selectInput('xcol', 'X Variable', data_list, selected = "flaeche"),
+                        selectInput('ycol', 'Y Variable', data_list, selected = "kalt")
+                        # selectInput('color', 'Einfärben', names(data), selected = "zimmer"),
+                        # sliderInput("kalt", "Kalt:",
+                        #             min = kalt["min"], max = kalt["max"],
+                        #             value = c(kalt["min"], kalt["max"])),
+                        # sliderInput("flaeche", "Fläche:",
+                        #             min = flaeche["min"], max = flaeche["max"],
+                        #             value = c(flaeche["min"], flaeche["max"])),
+                        # sliderInput("zimmer", "Zimmer:",
+                        #             min = zimmer["min"], max = zimmer["max"],
+                        #             value = c(zimmer["min"], zimmer["max"])),
+                        # selectInput('typ', 'Typ auswählen',
+                        #             state.name, multiple=TRUE, selectize=TRUE,
+                        #             choices = unique(data$typ)),
+                        # checkboxGroupInput('spider_name', 'Plattformen auswählen',
+                        #                    choices = unique(t$spider_name),selected = unique(t$spider_name)),
+                        # selectInput('criteria', 'Positiv-Kriterien TODO!!!!',
+                        #             state.name, multiple=TRUE, selectize=TRUE,
+                        #             choices = unique(gsub(' ','',unlist(strsplit(data$criteria, ","))))),
+                        # selectInput('zip', 'PLZ',
+                        #             state.name, multiple=TRUE, selectize=TRUE,
+                        #             choices = zips)
+                      ),
+                      mainPanel(
+                        # plotOutput('plot1', click = "plot_click", brush = "plot_brush"),
+                        # verbatimTextOutput("info"),
+                        # leafletOutput("mymap")
+                      ))
+             )
+  ) # navbarPage
+  
+)
